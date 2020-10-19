c8523afe60997dd1d62c8d657b264def93b5d024
zhangjrjlu <62231221+zhangjrjlu@users.noreply.github.com>
2020-10-19 10:38:33 +0800

    增加剑指Offer_42_.连续子数组的最大和.py

---
+++ 剑指Offer_42_.连续子数组的最大和.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i-1], 0) + nums[i]
+
+        return max(nums)
