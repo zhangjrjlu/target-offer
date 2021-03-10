4b2ab6e88d81dda3d5c131328310ecb90d332d48
zhangjrjlu <62231221+zhangjrjlu@users.noreply.github.com>
2021-03-10 16:37:23 +0800

    Update 剑指Offer__29__顺时针打印矩阵.py

--- 剑指Offer__29__顺时针打印矩阵.py
+++ 剑指Offer__29__顺时针打印矩阵.py
@@ -28,3 +28,27 @@ class Solution:
             j = j + directions[directionIndex][1]
         
         return order
+#         if not matrix or not matrix[0]:
+#             return list()
+        
+#         rows, columns = len(matrix), len(matrix[0])
+#         order = list()
+#         left, right, top, bottom = 0, columns-1, 0, rows-1
+#         while left<=right and top <= bottom:
+#             for column in range(left, right+1):
+#                 order.append(matrix[top][column])
+            
+#             for row in range(top+1, bottom+1):
+#                 order.append(matrix[row][right])
+            
+#             if left < right and top < bottom:
+#                 for c in range(right-1, left-1, -1):
+#                     order.append(matrix[bottom][c])
+#                 for r in range(bottom-1, top, -1):
+#                     order.append(matrix[r][left])
+                
+
+#             left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
+
+#         return order
+
