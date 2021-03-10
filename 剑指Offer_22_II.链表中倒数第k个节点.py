0f4a9b6c3c4ad78e8d1f1b050c52a7b6685a9bd4
zhangjrjlu <62231221+zhangjrjlu@users.noreply.github.com>
2021-03-10 16:35:46 +0800

    Update 剑指Offer_22_II.链表中倒数第k个节点.py

--- 剑指Offer_22_II.链表中倒数第k个节点.py
+++ 剑指Offer_22_II.链表中倒数第k个节点.py
@@ -7,3 +7,23 @@ class Solution:
             front = front.next
             last = last.next
         return last
+        class Solution:
+    
+#         n = 0
+#         f = head
+#         l = head
+#         while f:
+#             f = f.next
+#             n = n+1
+#         print (n)
+#         for i in range(n-k):
+#             l = l.next
+#         return l
+    
+        # former, latter = head, head
+        # for _ in range(k):
+        #     former = former.next
+        # while former:
+        #     former, latter = former.next, latter.next
+        # return latter
+
